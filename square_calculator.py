f85fca629c3fdd4713bbd9afc0f49de8968c878d
Mallak Hesham <M.Hesham2246@nu.edu.eg>
2024-11-14 01:19:25 +0200

    Create square_calculator.py

---
+++ square_calculator.py
@@ -0,0 +1,10 @@
+# square_calculator.py
+
+class SquareCalculator:
+    def area(self, side):
+        """Returns the area of the square (side^2)."""
+        return side * side
+
+    def perimeter(self, side):
+        """Returns the perimeter of the square (4 * side)."""
+        return 4 * side
